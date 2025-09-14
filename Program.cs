f6fc8c3319504a231899c1efbf0275bce7121484
Licaonchik <ereneger1609@gmail.com>
2025-09-14 20:12:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите длину а");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину b");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длину c");
+double c = Convert.ToDouble(Console.ReadLine());
+if (a == b && b == c)
+{
+    Console.WriteLine("Треугольник равнобедренный");
+}
+else
+{
+    Console.WriteLine("треугольник не равнобедренный");
+}
+
